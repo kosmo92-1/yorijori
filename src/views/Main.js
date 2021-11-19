22aa92c16faaad9e00382b583a0c827335a8ccb6
Cynthia Kim <raspberrylounge@gmail.com>
2021-11-19 15:48:59 +0900

    markup header and footer

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import banner from '../assets/images/banner.png';
 
-function Main(props) {
+function Main() {
     return (
-        <main>
+        <main id="main">
             <h2 className="sr-only">구독 중인 레시피, 추천 레시피 등을 소개하는 페이지</h2>
-            <section>banners</section>
+            <section class="banners">
+                <img src={banner} alt="딸기 에끌레르, 제철인 딸기를 넣은 달콤한 디저트" />
+            </section>
             <div className="">
                 <section>feeds</section>
                 <section>recommends</section>
