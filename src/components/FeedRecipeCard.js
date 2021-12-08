b3e00db62cfd0d5f423cb258ed0a4ac4ab2c98ed
yeokwang <yeokwang@gmail.com>
2021-12-08 11:17:37 +0900

    메인 영역 fix

--- src/components/FeedRecipeCard.js
+++ src/components/FeedRecipeCard.js
@@ -11,12 +11,12 @@ function FeedRecipeCard(props) {
         navigate(path)
     }
 
-    const url = `/recipe-detail/${recipes.value.RECIPE_ID}`
+    const url = `/recipe-detail/${recipes.value.recipe_id}`
     // const imgUrl = `yorijori/${recipes.value.recipe_thumbnail}`
     console.log(recipes)
     return (
        <a href={url} className="recipe-card" onClick={actionRead}>
-            <img src={recipes.value.RECIPE_THUMBNAIL} alt={recipes.value.RECIPE_TITLE} />
+            <img src={recipes.value.recipe_thumbnail} alt={recipes.value.recipe_title} />
             {/* {
                 recipes.map(({id, src, desc}) => <img key={id} src={src} alt={desc} />)
             } */}
