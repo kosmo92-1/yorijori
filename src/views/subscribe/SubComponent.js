e2c67cdc96dee03983a79e4192c36c4e548eb3af
yeokwang <yeokwang@gmail.com>
2021-12-09 01:22:30 +0900

    내 채널 -> 구독 -> 구독제목 클릭시 해당채널 이동 링크 추가

--- src/views/subscribe/SubComponent.js
+++ src/views/subscribe/SubComponent.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col } from 'reactstrap';
 
 function SubComponent(props) {
@@ -35,19 +36,32 @@ function SubComponent(props) {
         .catch(err =>{alert('실패')})
     }
 
-    const listComponent = sublist.map((item)=>(
-        <Col xs="4">
-        <Card key={item.channel_id}>
-            <CardImg top width="100%" src={item.channel_photo} alt="Card image cap" />
-            <CardBody>
-            <CardTitle tag="h5">{item.channel_name}</CardTitle>
-            {/* <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle> */}
-            <CardText>{item.channel_content}</CardText>
-            <Button onClick={()=>subscribeBtn(item.channel_id,sessionStorage.getItem('user_id'))}>구독취소</Button>
-            </CardBody>
-        </Card>
-        </Col>
-    ));
+    const listComponent = sublist.map((item) => (
+			<Col xs="4">
+				<Card key={item.channel_id}>
+					<CardImg
+						top
+						width="100%"
+						src={item.channel_photo}
+						alt="Card image cap"
+					/>
+					<CardBody>
+						<Link to={`/channel/${item.channel_id}`}>
+							<CardTitle tag="h5">{item.channel_name}</CardTitle>
+						</Link>
+							{/* <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle> */}
+							<CardText>{item.channel_content}</CardText>
+						<Button
+							onClick={() =>
+								subscribeBtn(item.channel_id, sessionStorage.getItem("user_id"))
+							}
+						>
+							구독취소
+						</Button>
+					</CardBody>
+				</Card>
+			</Col>
+		));
 
 
 
