20dc3ce8d9f213cc0bec9f961a3c66b810de28c0
JeonHyeIn <skehskeh002@gmail.com>
2021-12-08 11:03:06 +0900

    FeedRecipeCard.js 수정

--- src/components/FeedRecipeCard.js
+++ src/components/FeedRecipeCard.js
@@ -11,12 +11,12 @@ function FeedRecipeCard(props) {
         navigate(path)
     }
 
-    const url = `/recipe-detail/${recipes.value.recipe_id}`
+    const url = `/recipe-detail/${recipes.value.RECIPE_ID}`
     // const imgUrl = `yorijori/${recipes.value.recipe_thumbnail}`
-
+    console.log(recipes)
     return (
        <a href={url} className="recipe-card" onClick={actionRead}>
-            <img src={recipes.value.recipe_thumbnail} alt={recipes.value.recipe_title} />
+            <img src={recipes.value.RECIPE_THUMBNAIL} alt={recipes.value.RECIPE_TITLE} />
             {/* {
                 recipes.map(({id, src, desc}) => <img key={id} src={src} alt={desc} />)
             } */}
