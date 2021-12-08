cbef9a4a2abd3fdd8f7a877f94b15e50b3359196
yeokwang <yeokwang@gmail.com>
2021-12-09 02:25:52 +0900

    공지상세 수정

--- src/views/notice/ReadNotice.js
+++ src/views/notice/ReadNotice.js
@@ -1,14 +1,13 @@
-import { TextField } from '@mui/material'
-import { minWidth } from '@mui/system'
 import axios from 'axios'
-import React, { useEffect, useState, createRef } from 'react'
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Container, FormGroup, Input, Label, Table } from 'reactstrap'
+import React, { useEffect, useState } from 'react'
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
 
-const ReadNotice = () => {
+const ReadNotice = ( {history} ) => {
   const [title, setTitle] = useState('title')
   const [content, setContent] = useState('content')
   const [head, setHead] = useState('공지사항')
   const [id, setId] = useState(0)
+  const [regdate, setRegdate] = useState(0)
 
   const noticeJson = {
     notice_id: id,
@@ -34,51 +33,81 @@ const ReadNotice = () => {
         setContent(res.data.read.notice_content)
         setHead(res.data.read.notice_head)
         setId(res.data.read.notice_id)
+        setRegdate(res.data.read.notice_regdate)
       })
       .catch((err) => {
         alert('실패')
       })
   }, [])
-  return (
-    <Container maxWidth="sm">
 
-          <Label tag="h5">공지상세보기</Label>
-          <Label className="mb-2 text-muted" tag="h6">
-          공지사항 게시판입니다.
-          </Label>
-            <FormGroup className="mb-3">
-            <TextField
-                  id="standard-multiline-flexible"
-                  label="공지유형"
-                  multiline
-                  maxRows={4}
-                  value={head}
-                  variant="standard"
-              />
-            </FormGroup>
-            <FormGroup>
-              <TextField
-                  id="standard-multiline-flexible"
-                  label="제목"
-                  multiline
-                  maxRows={5}
-                  value={title}
-                  variant="standard"
-              />
-            </FormGroup>
-            <FormGroup >
-              <TextField
-                  id="standard-multiline-flexible"
-                  label="내용"
-                  multiline
-                  minRows={20}
-                  sx={{ minWidth: 1000 }}
-                  value={content}
-                  variant="standard"
-        />
-            </FormGroup>
-    </Container>
-  )
+  return (
+		<ListGroup>
+			<ListGroupItem>
+				<ListGroupItemHeading>공지상세보기</ListGroupItemHeading>
+				<ListGroupItemText>공지사항 게시판입니다.</ListGroupItemText>
+			</ListGroupItem>
+			<ListGroupItem>
+				
+			</ListGroupItem>
+			<ListGroupItem>
+				<ListGroupItemHeading>공지유형</ListGroupItemHeading>
+				<ListGroupItemText>{head}</ListGroupItemText>
+			</ListGroupItem>
+			<ListGroupItem>
+				<ListGroupItemHeading>제목</ListGroupItemHeading>
+				<ListGroupItemText>{title}</ListGroupItemText>
+			</ListGroupItem>
+			<ListGroupItem>
+				<ListGroupItemHeading>내용</ListGroupItemHeading>
+				<ListGroupItemText>{content}</ListGroupItemText>
+			</ListGroupItem>
+			<ListGroupItem>
+				<ListGroupItemHeading>등록일</ListGroupItemHeading>
+				<ListGroupItemText>
+					{new Date(regdate).toLocaleDateString()}
+				</ListGroupItemText>
+			</ListGroupItem>
+		</ListGroup>
+
+		// <Container maxWidth="xs">
+
+		//       <Label tag="h5">공지상세보기</Label>
+		//       <Label className="mb-2 text-muted" tag="h6">
+		//       공지사항 게시판입니다.
+		//       </Label>
+		//         <FormGroup className="mb-3 xs">
+		//         <TextField
+		//               id="standard-multiline-flexible"
+		//               label="공지유형"
+		//               multiline
+		//               maxRows={4}
+		//               value={head}
+		//               variant="standard"
+		//           />
+		//         </FormGroup>
+		//         <FormGroup>
+		//           <TextField
+		//               id="standard-multiline-flexible"
+		//               label="제목"
+		//               multiline
+		//               maxRows={5}
+		//               value={title}
+		//               variant="standard"
+		//           />
+		//         </FormGroup>
+		//         <FormGroup >
+		//           <TextField
+		//               id="standard-multiline-flexible"
+		//               label="내용"
+		//               multiline
+		//               minRows={20}
+		//               sx={{ minWidth: 1000 }}
+		//               value={content}
+		//               variant="standard"
+		//     />
+		//         </FormGroup>
+		// </Container>
+	);
 }
 
 export default ReadNotice;
